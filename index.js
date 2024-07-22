d0160befac01977b9f70fdb2e81a2e56608f959f
Anna <anna.vasiljeva94@gmail.com>
2024-07-22 21:26:51 +0300

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 'use strict';
-let number = prompt("Please enter a three-digit number:");
-if (number.length === 3 && !isNaN(number)) {
-    const digits = number.split('').map(Number);
-    const [digit1, digit2, digit3] = digits;
-    const allSame = digit1 === digit2 && digit2 === digit3;
-    const anySame = digit1 === digit2 || digit1 === digit3 || digit2 === digit3;
-    if (allSame) {
-        alert("All digits are the same");
-    } else if (anySame) {
-        alert("There are some identical digits");
-    } else {
-        alert("All digits are different");
+const sportsChampions = {
+    футбол: "Ліонель Мессі",
+    баскетбол: "Майкл Джордан",
+    теніс: "Роджер Федерер"
+};
+const birthYear = prompt("Будь ласка, введи рік свого народження:");
+if (birthYear && !isNaN(birthYear) && Number.isInteger(Number(birthYear)) && birthYear.length === 4 && birthYear <= 2024) {
+    const city = prompt("В якому місті ти живеш?");
+    if (city) {
+        const favoriteSport = prompt("Який твій улюблений вид спорту?");
+        if (favoriteSport) {
+            const currentYear = new Date().getFullYear();
+            const age = currentYear - birthYear;
+            let cityMessage;
+            if (city.toLowerCase() === "київ") {
+                cityMessage = "Ти живеш у столиці України.";
+            } 
+            else if (city.toLowerCase() === "вашингтон") {
+                cityMessage = "Ти живеш у столиці США.";
+            } 
+            else if (city.toLowerCase() === "лондон") {
+                cityMessage = "Ти живеш у столиці Великої Британії.";
+            } 
+            else {
+                cityMessage = "Ти живеш у місті " + city + ".";
+            }
+            let sportMessage;
+            const sportKey = favoriteSport.toLowerCase();
+            if (sportsChampions[sportKey]) {
+                sportMessage = "Круто! Хочеш стати як " + sportsChampions[sportKey] + "?";
+            } 
+            else {
+                sportMessage = "Твій улюблений вид спорту: " + favoriteSport;
+            }
+            alert("Твій вік: " + age + "\n" + cityMessage + "\n" + sportMessage);
+        } 
+        else {
+            alert("Шкода, що ти не захотів ввести свій улюблений вид спорту.");
+        }
+    } 
+    else {
+        alert("Шкода, що ти не захотів ввести своє місто.");
     }
-} else {
-    alert("Please enter a valid three-digit number.");
+} 
+else {
+    alert("Шкода, що ти не захотів ввести свій рік народження.");
 }
-    
-
-
-
-
-
-
-
-
-
-
-
 
